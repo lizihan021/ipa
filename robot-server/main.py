1e9ab537683766676d51dff2a0b21f0783e90a73
lizihan <lizihan@umich.edu>
2017-11-09 23:26:59 -0500

    yo

--- robot-server/main.py
+++ robot-server/main.py
@@ -116,7 +116,7 @@ if __name__ == '__main__':
     # try to put ip on the server
     print("Try to put ip on the server")
     try:
-        urllib2.urlopen("52.39.88.90:3000/setip/"+robot_id+'/'+robot_ip)
+        urllib2.urlopen("www.sjtusaa.com/setip/"+robot_id+'/'+robot_ip)
     except:
         print("Fail to put ip on server.")
 

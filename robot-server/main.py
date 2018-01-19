3ca3756e655330f365ab7d5e5f9f1092b2efdd83
lizihan <lizihan@umich.edu>
2018-01-18 19:06:46 -0500

    yo

--- robot-server/main.py
+++ robot-server/main.py
@@ -122,7 +122,7 @@ def gen():
     counter = 0
     while True:
         try:
-            time.sleep(0.1)
+            time.sleep(5)
             array,_ = freenect.sync_get_video()
             array = cv2.cvtColor(array,cv2.COLOR_RGB2BGR)
             output = array

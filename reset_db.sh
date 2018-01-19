eff23ca796a7736edc7018f726d20cbee03d75ed
lizihan <lizihan@umich.edu>
2018-01-18 19:36:56 -0500

    yo

--- reset_db.sh
+++ reset_db.sh
@@ -1,4 +1,4 @@
-rm ipa_middle_server/model/robot.sqlite
-sqlite3 ipa_middle_server/model/robot.sqlite < ipa_middle_server/sql/schema.sql
-sqlite3 ipa_middle_server/model/robot.sqlite < ipa_middle_server/sql/data.sql
+rm ipa-middle-server/model/robot.sqlite
+sqlite3 ipa-middle-server/model/robot.sqlite < ipa-middle-server/sql/schema.sql
+sqlite3 ipa-middle-server/model/robot.sqlite < ipa-middle-server/sql/data.sql
 echo "reset finish!"

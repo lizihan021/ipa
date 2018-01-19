2ffa5c98896ead990e4d4b6196385f2f33211ad1
lizihan <lizihan@umich.edu>
2018-01-18 20:13:10 -0500

    yo

--- robot-server/main.py
+++ robot-server/main.py
@@ -103,9 +103,10 @@ def index():
     return render_template('index.html')
 
 def someFunc(jpeg):
-    # with tempfile.TemporaryFile() as t:
-    #     t.write(jpeg)
-    r = requests.post('http://35.0.30.117:3000/api/Upload', files={'imgUploader': jpeg}, \
+    with tempfile.TemporaryFile() as t:
+        t.write(jpeg)
+        t.flush()
+        r = requests.post('http://35.0.30.117:3000/api/Upload', files={'imgUploader': t}, \
             data={'filename':"yo.jpg"})
     #time.sleep(2)
 
